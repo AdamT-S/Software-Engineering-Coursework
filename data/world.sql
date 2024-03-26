2dd4c62b8e2b84d6f3cbc606d6f778f3cb61284e
Athenaparsa <athena.parsa1@gmail.com>
2024-03-26 00:05:23 +0000

    1st commit

--- data/world.sql
+++ data/world.sql
@@ -30,7 +30,7 @@ CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`),``
   UNIQUE INDEX email_index (`email`)
 );
 --
