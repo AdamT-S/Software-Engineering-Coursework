b3c3a68f28da2212a0a1e1ff396fa15d88c00663
AdamT-S <158591813+AdamT-S@users.noreply.github.com>
2024-03-26 08:53:57 +0000

    Added a function to get a single continent

--- database services/databaseservices.mjs
+++ database services/databaseservices.mjs
@@ -162,6 +162,22 @@ export default class DatabaseService {
             return [];
         }
     }
+     // SQL statement that gets all of the continents
+     async getContinent(Continent) {
+        try {
+            const sql = `
+            SELECT Continent, SUM(country.Population) AS Population
+            FROM country
+            WHERE Continent = "Europe"
+            GROUP BY Continent
+            `;
+            const [rows, fields] = await this.conn.execute(sql);
+            return rows;
+        } catch (err) {
+            console.error("Error fetching continents:", err);
+            return [];
+        }
+    }
     
     // SQL statement that deletes continents
     async deleteContinent(continentName) {
